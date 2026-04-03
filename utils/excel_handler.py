edc72dd38e8cf814c34bb4a50e381f0699ff0139
careatc1-pixel <care.atc1@gmail.com>
2026-04-03 15:18:54 +0530

    Update excel_handler.py

--- utils/excel_handler.py
+++ utils/excel_handler.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from models import Client, db
 
+def upload_clients_from_excel(file):
+    df = pd.read_excel(file)
+
+    for _, row in df.iterrows():
+        client = Client(
+            name=row['name'],
+            email=row['email'],
+            phone=row['phone']
+        )
+        db.session.add(client)
+
+    db.session.commit()
