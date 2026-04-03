84d7437a58c6e9f476e7343f90eb8437f4c92736
careatc1-pixel <care.atc1@gmail.com>
2026-04-03 15:21:35 +0530

    Update excel_handler.py

--- utils/excel_handler.py
+++ utils/excel_handler.py
@@ -1,7 +1,24 @@
 import pandas as pd
-from models import Client, db
 
-def upload_clients_from_excel(file):
+def export_clients_to_excel(Client):
+    clients = Client.query.all()
+    data = []
+
+    for c in clients:
+        data.append({
+            "Name": c.name,
+            "Email": c.email,
+            "Phone": c.phone
+        })
+
+    file_path = "clients.xlsx"
+    df = pd.DataFrame(data)
+    df.to_excel(file_path, index=False)
+
+    return file_path
+
+
+def upload_clients_from_excel(file, Client, db):
     df = pd.read_excel(file)
 
     for _, row in df.iterrows():
